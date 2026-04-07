e7cc07d8186f2218ca0781a5469fc266cde84538
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-04-07 09:38:41 -0600

    Se termino el login de administador

--- client-admin/src/app/App.jsx
+++ client-admin/src/app/App.jsx
@@ -1,9 +1,11 @@
 import { AuthPage } from "../features/auth/pages/AuthPage.jsx";
+import { LoginForm } from "../features/auth/components/LoginForm.jsx";
 function App() {
     return (
         <>
           <h1>Hola Mundo</h1>
           <AuthPage />
+          <LoginForm />
         </>
   );
 }
