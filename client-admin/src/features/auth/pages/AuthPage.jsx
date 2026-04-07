62837a2d7793e2e154e9f4614f3119dda2668552
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-04-07 08:51:15 -0600

    Modificacion del diseño de la pagina

--- client-admin/src/features/auth/pages/AuthPage.jsx
+++ client-admin/src/features/auth/pages/AuthPage.jsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
 
 const AuthPage = () => {
-    
-    const [isLogin, setisLogin] = useState(true);
-    const [isForgot, setisForgot] = useState(false);
+    const [isLogin, setIsLogin] = useState(true);
+    const [isForgot, setIsForgot] = useState(false);
 
     return(
+        
+        <div className="w-full max-w-xl bg-white rounded-xl shadow-lg 
+        border border-gray-200 p-6 md: p-10">
+        
+            {/* Logo */}
+            <div className="flex justify-center mb-6">
+                    <img
+                    src="/src/assets/img/kinal_sports.png"
+                    alt="Kinal Sport"
+                    className="h-20 w-auto"
+                    />
+                </div>
 
-        <h1>Auth Page</h1>
+                <div className=" text-center mb-6">
+
+                    <h1 className="text-2xl lg:text-3xl font-bold
+                    text-gray-900 mb-2">
+
+                    {isForgot
+                    ? "Recuperar Contraseña"
+                    :isLogin
+                    
+                    }
+                    
+                    </h1>
+
+                </div>
+
+        </div>
 
     );
 };
