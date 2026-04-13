7574605bef95ac504bd9806be0d272b7e2f14a7d
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-04-13 09:14:47 -0600

    Implementacion del dashboard

--- client-admin/src/app/App.jsx
+++ client-admin/src/app/App.jsx
@@ -1,11 +1,22 @@
-import { AuthPage } from "../features/auth/pages/AuthPage.jsx";
-import { LoginForm } from "../features/auth/components/LoginForm.jsx";
-import { ForgotPasswordForm } from "../features/auth/components/ForgotPasswordForm.jsx";
+import { AppRoutes } from "./router/AppRoutes.jsx";
+import { Toaster } from "react-hot-toast";
+
 function App() {
     return (
         <>
-          <h1>Hola Mundo</h1>
-          <AuthPage />
+          <Toaster
+          position="top-center"
+          toastOptions={{
+            style: {
+              fontFamily: "inherit",
+              fontWeight: "600",
+              fontSize: "1rem",
+              borderRadius: "8px",
+            }
+          }}
+          />
+
+          <AppRoutes />
         </>
   );
 }
---
+++ client-admin/src/app/layouts/DashboardPage.jsx
@@ -0,0 +1,8 @@
+import { DashboardContainer } from "../../shared/components/layout/DashboardContainner";
+
+export const DashboardPage = () => {
+
+    return(
+        <DashboardContainer />
+    );
+}
